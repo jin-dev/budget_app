9bc9b3d52151f81a1d0a2ed0435a94ad162fbede
Jin Park <jinwoo.park@utransfer.com>
2020-12-10 16:28:42 +0900

    fixed deleteTransaction hooks

--- src/components/IncomeTransaction.js
+++ src/components/IncomeTransaction.js
@@ -5,18 +5,17 @@ const IncomeTransaction = ({ incomeTransaction }) => {
     const { deleteTransaction } = useContext (GlobalContext);
     return (
         <li className="transaction">
-            <span className="transaction-text">
-                {incomeTransaction.incomeText}
-            </span>
-            <span className="transaction-amount">
-                {incomeTransaction.incomeAmount}
-            </span>
-            <button className="delete-btn"
-             onClick={() => deleteTransaction(incomeTransaction.id)}
-            >
-                <i className="fas-fa-trash"></i>
-            </button>
-        </li>
+        <span className="transaction-text">{incomeTransaction.incomeText}</span>
+        <span className="transaction-amount">
+          ${incomeTransaction.incomeAmount}
+        </span>
+        <button
+          onClick={() => deleteTransaction(incomeTransaction.id)}
+          className="delete-btn"
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
     )
 }
 
