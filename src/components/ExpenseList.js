f6ad09c2c9b2e8ef0ef8104b5ae68730939d1232
Jin Park <jinwoo.park@utransfer.com>
2020-12-10 16:19:36 +0900

    updated tutorial parts

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,29 @@
+import React, { useContext }  from 'react'
+import { GlobalContext } from "../context/GlobalState";
+import ExpenseTransaction from './ExpenseTransaction';
+
+const ExpenseList = () => {
+
+    const { expenseTransactions } = useContext(GlobalContext);
+
+    console.log("The Expense Value : " , expenseTransactions);
+
+    return (
+        <div className="transactions transactions-expense">
+            <h2>transaction History</h2>
+            <ul className="transaction-list">
+               {expenseTransactions.map(expenseTransaction => (
+                   
+                   <ExpenseTransaction key={expenseTransaction.id} 
+                   expenseTransaction = {expenseTransaction} />
+
+               ))}
+            </ul>
+
+            
+        </div>
+    )
+}
+
+export default ExpenseList;
+ 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>BUDGET APP</h1>
+            
+        </div>
+    )
+}
+
+export default Header
